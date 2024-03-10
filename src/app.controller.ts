5f63cfa06ce2af977dcd8787829af5dce58d584f
OSVALDO <Osvavazdelatorre@gmail.com>
2024-03-10 00:53:06 -0600

    delete images

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,8 @@
-import { Controller, Get, Res } from '@nestjs/common';
-import { AppService } from './app.service';
-import { Response } from 'express';
+import { Injectable } from '@nestjs/common';
 
-@Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(@Res() response: Response) {
-    response.send(this.appService.getHello());
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'HOLA AMIGOS';
   }
 }
