2cf118a958581a2127e4ef6ea3222671c5babffa
OSVALDO <Osvavazdelatorre@gmail.com>
2024-03-09 23:06:46 -0600

    Manejo de errores

--- src/services/batch-logger/batch-logger.service.ts
+++ src/services/batch-logger/batch-logger.service.ts
@@ -1,22 +1,21 @@
 import { Injectable, OnModuleDestroy } from '@nestjs/common';
 import { PrismaService } from '../prisma.services';
 
-
 @Injectable()
 export class BatchLoggerService implements OnModuleDestroy {
-  private dataStore: Map<string, Set<string>> = new Map(); 
+  private dataStore: Map<string, Set<string>> = new Map();
 
   constructor(private prismaService: PrismaService) {
     setInterval(() => {
       this.writeBatchToDatabase();
-    }, 60000); 
+    }, 60000);
   }
 
   public logRequest(userId: string, ip: string): void {
     if (!this.dataStore.has(userId)) {
       this.dataStore.set(userId, new Set());
     }
-    this.dataStore.get(userId).add(ip);
+    this.dataStore.get(userId)?.add(ip);
   }
 
   private async writeBatchToDatabase(): Promise<void> {
@@ -25,20 +24,25 @@ export class BatchLoggerService implements OnModuleDestroy {
     this.dataStore.forEach((ips, userId) => {
       ips.forEach((ip) => {
         records.push({
-          userId,
-          ip,
-          createdAt: new Date(), 
+          userId: parseInt(userId, 10), 
+          createdAt: new Date(),
         });
       });
     });
 
-    try {
-      await this.prismaService.iPRecord.createMany({
-        data: records,
-        skipDuplicates: true, 
-      });
-    } catch (error) {
-      console.error('Error al escribir en la base de datos', error);
+    if (records.length > 0) {
+      try {
+        await this.prismaService.iPRecord.createMany({
+          data: records,
+          skipDuplicates: true,
+        });
+      } catch (error) {
+        if (error.code === 'P2003') {
+          console.error('Foreign key constraint failed, userId not found in the database.');
+        } else {
+          console.error('Error writing to the database', error);
+        }
+      }
     }
 
     this.dataStore.clear();
