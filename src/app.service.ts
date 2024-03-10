e0576a65d58602e39c6ea6a56d7c0bc901491b35
OSVALDO <Osvavazdelatorre@gmail.com>
2024-03-10 15:06:58 -0600

    images

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,29 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'HOLA AMIGOS';
+    return `
+      <!DOCTYPE html>
+      <html>
+      <head>
+          <title>Fondo de Pantalla Completa</title>
+          <style>
+              body, html {
+                  height: 100%;
+                  margin: 0;
+              }
+              .background {
+                  background-image: url('https://www.kuroro.com/build/_assets/bb-poster-square-BBNXDVUH.png');
+                  background-position: center;
+                  background-repeat: no-repeat;
+                  background-size: cover;
+                  height: 100%;
+              }
+          </style>
+      </head>
+      <body class="background">
+      </body>
+      </html>
+    `;
   }
+  
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { join } from 'path';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use('/images', express.static(join(__dirname, '..', 'images')));
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
+
