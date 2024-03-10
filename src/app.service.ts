ae002473c5564a551c9fcb1e214b1346ab3c1736
OSVALDO <Osvavazdelatorre@gmail.com>
2024-03-10 00:03:15 -0600

    images

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,29 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'HOLA AMIGOS';
+    return `
+      <!DOCTYPE html>
+      <html>
+      <head>
+          <title>Fondo de Pantalla Completa</title>
+          <style>
+              body, html {
+                  height: 100%;
+                  margin: 0;
+              }
+              .background {
+                  background-image: url('/images/KURORO-BACK.png');
+                  background-position: center;
+                  background-repeat: no-repeat;
+                  background-size: cover;
+                  height: 100%;
+              }
+          </style>
+      </head>
+      <body class="background">
+      </body>
+      </html>
+    `;
   }
+  
 }
